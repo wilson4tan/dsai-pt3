cef25919a7191eaa97881b15b05ceb6818a93c9e
wilson4tan <100810635+wilson4tan@users.noreply.github.com>
2026-03-02 20:22:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import datetime
 
 # for AWS, do not run this because not using .env
 #os.environ["GROQ_API_KEY"] = ""
-#os.environ["GROQ_API_KEY"] = os.environ.get('GROQ_API_KEY')
+os.environ["GROQ_API_KEY"] = os.environ.get('GROQ_API_KEY')
 
 client = Groq()
 
